df11dee024fbff1bf3694cdf908d8c1041e99a61
Max Gardner <max.c.gardner@gmail.com>
2017-08-21 14:29:52 -0500

    Better menu functionality for tablets, added footer, embedded map, and buttons

--- assets/js/site.js
+++ assets/js/site.js
@@ -2,32 +2,38 @@ $(document).ready(() => {
     // Set a scroll position for certain sections so we can
     // recognize when user has gotten to that area
 
-    $(".hero:after").scrollTop(1);
+    $('.hero:after').scrollTop(1);
 
     $(window).scroll(function() {
         if ($(window).scrollTop() >= 1) {
-            $(".navbar").addClass("affix").fadeIn(10000);        
+            $('.navbar').addClass('affix').fadeIn(10000);        
         }
     
         if ($(window).scrollTop() < 1) {
-            $(".navbar").removeClass("affix").fadeIn(10000);
+            $('.navbar').removeClass('affix').fadeIn(10000);
         }
     });
 
-    $(document).on("click", ".expand-nav", () => {
-        switch ($("nav").css("display")) {
-            case "none":
-                $("nav").css("display", "block");
-                $(".navbar").css("background-color", "#FFF");                
+    $(document).on('click', '.expand-nav', () => {
+        switch ($('nav').css('display')) {
+            case 'none':
+                $('nav').css('display', 'block');
+                $('.navbar').css('background-color', '#FFF');                
                 break;
             default:
-                $("nav").css("display", "none");
-                $(".navbar").css("background-color", "");                                
+                $('nav').css('display', 'none');
+                $('.navbar').css('background-color', '');                                
                 break;
         }
     });
-    
-    $("#about").mouseover(() => {
-        $(".light").toggleClass("hidden");
-      });
+
+    if ($('.expand-nav').css('display') === "block") {
+        $(document).on('click', 'nav *', () => {
+            $('nav').css('display', 'none');
+        });
+    }
+
+    $('#about .container').hover(() => {
+        $('#lightbulb').fadeToggle('fast', 0);
+    })
 });
