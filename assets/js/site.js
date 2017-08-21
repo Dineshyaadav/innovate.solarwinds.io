65123d75f808793d8db2fe4c5cfea8144e00b515
Max Gardner <max.c.gardner@gmail.com>
2017-08-21 16:53:31 -0500

    Added js and css to highlight active nav link

--- assets/js/site.js
+++ assets/js/site.js
@@ -5,13 +5,56 @@ $(document).ready(() => {
     $('.hero:after').scrollTop(1);
 
     $(window).scroll(function() {
-        if ($(window).scrollTop() >= 1) {
-            $('.navbar').addClass('affix').fadeIn(10000);        
+        let scroll = $(window).scrollTop();
+        if (scroll >= 1) {
+            $('.navbar').addClass('affix');        
+        } else {
+            $('.navbar').removeClass('affix');
         }
-    
-        if ($(window).scrollTop() < 1) {
-            $('.navbar').removeClass('affix').fadeIn(10000);
+
+        // Highlight active nav links
+        let $about = $('section#about');
+        let $schedule = $('section#schedule');
+        let $register = $('section#register');
+        let $speakers = $('section#speakers');
+        let $details = $('section#details');
+        let navHeight = $('.navbar').outerHeight(true);       
+
+        if (scroll >= ($about.offset().top - navHeight)
+            && scroll <= ($about.offset().top + $about.outerHeight()/2 - navHeight)) {
+            $('nav li a[href="#about"]').addClass('active');
+        } else {
+            $('nav li a[href="#about"]').removeClass('active');
         }
+
+        if (scroll >= $schedule.offset().top
+            && scroll <= ($schedule.offset().top + $schedule.outerHeight()/2 - navHeight)) {
+            $('nav li a[href="#schedule"]').addClass('active');
+        } else {
+            $('nav li a[href="#schedule"]').removeClass('active');
+        }
+
+        if (scroll >= $register.offset().top
+            && scroll <= ($register.offset().top + $register.outerHeight()/2 - navHeight)) {
+            $('nav li a[href="#register"]').addClass('active');
+        } else {
+            $('nav li a[href="#register"]').removeClass('active');
+        }
+
+        if (scroll >= $speakers.offset().top
+            && scroll <= ($speakers.offset().top + $speakers.outerHeight()/2 - navHeight)) {
+            $('nav li a[href="#speakers"]').addClass('active');
+        } else {
+            $('nav li a[href="#speakers"]').removeClass('active');
+        }
+
+        if (scroll >= $details.offset().top
+            && scroll <= ($details.offset().top + $details.outerHeight()/2 - navHeight)) {
+            $('nav li a[href="#details"]').addClass('active');
+        } else {
+            $('nav li a[href="#details"]').removeClass('active');
+        }
+
     });
 
     $(document).on('click', '.expand-nav', () => {
